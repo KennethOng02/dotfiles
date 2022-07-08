10059425dcfe2ca47506dbe3da68aefd295d9f57
KennethOng02 <kennethongkp@gmail.com>
2022-07-09 00:13:37 +0800

    initial lua config

---
+++ .config/nvim/lua/pluto/options.lua
@@ -0,0 +1,29 @@
+-- :help options
+local options = {
+  autochdir = true,
+  autoindent = true,
+  background = "dark",
+  backup = false,
+  clipboard = "unnamedplus",
+  cursorline = true,
+  expandtab = true,		        -- convert tabs to spaces
+  fileencoding = "utf-8",
+  ignorecase = true,
+  mouse = "a",
+  number = true,
+  scrolloff = 8,
+  shiftwidth = 2,			-- default: 8, num of spaces used for (auto)indent
+  sidescrolloff = 8,
+  smartcase = true,
+  smartindent = true,
+  splitbelow = true,
+  splitright = true,
+  showtabline = 2,
+  syntax = "on",
+  undofile = true,
+  wrap = false,
+}
+
+for k, v in pairs(options) do
+  vim.opt[k] = v
+end
