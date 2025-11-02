8781bff01bb7fa73b11a90b5ee5bbe030adc0121
KennethOng02 <kennethongkp@gmail.com>
2025-11-02 23:11:48 +0800

    update: nvim file explorer hidden file

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -24,7 +24,12 @@ require("lazy").setup({
 
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.python" },
+    { import = "lazyvim.plugins.extras.lang.vue" },
     { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.docker" },
+    { import = "lazyvim.plugins.extras.lang.java" },
+    { import = "lazyvim.plugins.extras.lang.yaml" },
+    { import = "lazyvim.plugins.extras.lang.prisma" },
     { import = "lazyvim.plugins.extras.lang.toml" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
@@ -33,6 +38,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
 
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
+    { import = "lazyvim.plugins.extras.editor.mini-files" },
 
     { import = "plugins" },
   },
---
+++ .config/nvim/lua/plugins/file-explorer.lua
@@ -0,0 +1,16 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    notifier = { enabled = true },
+
+    picker = {
+      sources = {
+        explorer = {
+          hidden = true,
+          ignored = true,
+          exclude = { "node_modules", ".git" },
+        },
+      },
+    },
+  },
+}
