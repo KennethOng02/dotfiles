442dc88db981dfc1948df4b8133d3fdcc847f476
王關平 <kennethong@wisdomgarden.com>
2025-11-07 17:39:28 +0800

    add: vim copy current file path

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -6,3 +6,18 @@ local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
 
 keymap.set("i", "jk", "<Esc>", opts)
+
+keymap.set("n", "<leader>y", function()
+  local filepath = vim.fn.expand("%:p")
+  local root = vim.fn.getcwd() -- fallback if root detection fails
+
+  -- Try Git root
+  local git_root = vim.fn.systemlist("git rev-parse --show-toplevel")[1]
+  if vim.v.shell_error == 0 and git_root ~= "" then
+    root = git_root
+  end
+
+  -- Compute relative path
+  local relpath = vim.fn.fnamemodify(filepath, ":." .. root)
+  vim.fn.setreg("+", relpath)
+end, opts)
