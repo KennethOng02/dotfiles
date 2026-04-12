c1533d8c30b3939ff3677bdbfdb8c26c194940d5
KennethOng02 <kennethongkp@gmail.com>
2026-04-12 13:40:37 +0800

    update: zsh cf function

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -5,6 +5,8 @@ cf() {
         -type d \( -name 'node_modules' -o -name 'dist' -o -name '.turbo' -name 'target' -name '.git' \) -prune \
         -o \
         -type f \
+          ! -name '__init__.py' \
+          ! -name '__pycache_' \
           ! -name '.DS_Store' \
           ! -name 'LICENSE' \
           ! -name 'LICENSES' \
@@ -17,11 +19,15 @@ cf() {
           -print0 \
       | while IFS= read -r -d $'\0' file; do
         echo "===== $file ====="
+        echo '```'
         /bin/cat "$file"
+        echo '```'
       done
     elif [ -f "$path" ]; then
       echo "===== $path ====="
+      echo '```'
       /bin/cat "$path"
+      echo '```'
     else
       echo "Error: $path not found"
     fi
